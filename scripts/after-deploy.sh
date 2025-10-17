33edfb6b650acbc9bed55e38e9de6ae0fd30260a
y-minion <dudals2963@tukorea.ac.kr>
2025-10-17 21:29:02 +0900

    npm  도구 오바르게 사용 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,9 +2,9 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
-sudo ln -s /home/ubuntu/.nvm/versions/node/v22.20.0/bin/yarn /usr/bin/npm
+sudo ln -s /home/ubuntu/.nvm/versions/node/v22.20.0/bin/npm /usr/bin/npm
 sudo ln -s /home/ubuntu/.nvm/versions/node/v22.20.0/bin/pm2 /usr/bin/pm2
 
-sudo /usr/bin/npm ci
+sudo /usr/bin/npm install
 
-sudo /usr/bin/pm2 start yarn --name test-app -- start
+sudo /usr/bin/pm2 start npm --name test-app -- start
