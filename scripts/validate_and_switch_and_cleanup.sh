8c49ff69793fdc55db3fa1aef151f815961134fd
y-minion <dudals2963@tukorea.ac.kr>
2025-10-20 00:58:03 +0900

    💚 기존의  proxy.conf파일을 덮어씌우도록 스크립트 수정

--- scripts/validate_and_switch_and_cleanup.sh
+++ scripts/validate_and_switch_and_cleanup.sh
@@ -18,7 +18,7 @@ for i in {1..10}; do
         IDLE_PORT=$(grep -oP '(?<=:)\d+' /etc/nginx/conf.d/proxy.conf)
 
         echo ">>> [Step 2] Switching Nginx to Green Port: $GREEN_PORT"
-        echo "proxy_pass http://127.0.0.1:$GREEN_PORT;" >> /etc/nginx/conf.d/proxy.conf
+        echo "proxy_pass http://127.0.0.1:$GREEN_PORT;" | sudo tee /etc/nginx/conf.d/proxy.conf
 
         sudo systemctl reload nginx
         echo ">>> [Success] Nginx reloaded. Traffic is now served by Green server."
