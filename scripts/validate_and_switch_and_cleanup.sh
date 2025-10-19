783d811ebebfdea5d4886ed76783f87ae4a84727
y-minion <dudals2963@tukorea.ac.kr>
2025-10-20 00:55:27 +0900

    👷 그린서버 헬스체크 + 교체 스크립트 작성
    새로운 그린 환경의 헬스체크 실행하여 성공시 기존의 블루 환경을 정리하는 스크립트 작성

---
+++ scripts/validate_and_switch_and_cleanup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ ! -f /tmp/green_port.txt ]; then
+   echo  ">>> [Error]Could not find port file. The start_server step may have failed."
+   exit 1
+fi
+
+GREEN_PORT=$(cat /tmp/green_port.txt)
+
+echo ">>> [Step 1] Health check for New Green Server on port $GREEN_PORT"
+
+for i in {1..10}; do
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:$GREEN_PORT/health)
+
+    if [ $RESPONSE_CODE -eq 200 ]; then
+        echo ">>> [Success] Health check successful."
+
+        IDLE_PORT=$(grep -oP '(?<=:)\d+' /etc/nginx/conf.d/proxy.conf)
+
+        echo ">>> [Step 2] Switching Nginx to Green Port: $GREEN_PORT"
+        echo "proxy_pass http://127.0.0.1:$GREEN_PORT;" >> /etc/nginx/conf.d/proxy.conf
+
+        sudo systemctl reload nginx
+        echo ">>> [Success] Nginx reloaded. Traffic is now served by Green server."
+
+        echo ">>> [Step 3] Stopping Old Blue server on port $IDLE_PORT"
+        pm2 stop "app-$IDLE_PORT"
+        pm2 delete "app-$IDLE_PORT"
+        pm2 save
+
+        exit 0
+
+    fi
+
+    echo ">>> Health check failed. Retrying ... ($i/10)"
+    sleep 1
+done
+
+echo ">>> [Error] Health check failed after all retries. Rolling back to Blue server."
+pm2 stop "app-$GREEN_PORT"
+pm2 delete "app-$GREEN_PORT"
+pm2 save
+exit 1
+
+
