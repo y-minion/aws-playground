46c9e168ae058adba63af2b8297816b17f436156
y-minion <dudals2963@tukorea.ac.kr>
2025-10-20 01:06:25 +0900

    🐛 타입 오류 수정
    헬스체크 api에서 사용하지 않는 request매개변수 삭제 완료

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET(request: Request) {
+export async function GET() {
   return NextResponse.json(
     {
       status: "ok",
