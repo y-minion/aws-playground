4ad13af9bc63e23ad446cbf675c71b5a025b01db
y-minion <dudals2963@tukorea.ac.kr>
2025-10-20 13:55:35 +0900

    쉘에서 대괄호 사용시 공백 추가_문법 수정

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,10 @@ NVM_BIN_PATH="/home/ubuntu/.nvm/versions/node/v22.20.0/bin"
 
 export PATH="$NVM_BIN_PATH:$PATH"
 
-cd /home/ubuntu/application
+cd /home/ubuntu/application || exit 1
 
-if [ ! -f /etc/nginx/conf.d/proxy.conf]; then
+
+if [ ! -f /etc/nginx/conf.d/proxy.conf ]; then
     CURRENT_BLUE_PORT=3001
 else
     CURRENT_BLUE_PORT=$(grep -oP '(?<=:)\d+' /etc/nginx/conf.d/proxy.conf)
